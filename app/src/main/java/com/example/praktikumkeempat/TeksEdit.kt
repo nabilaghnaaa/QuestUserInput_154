ec169347c56cb82dc478d75047e31feff60592ca
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-31 09:24:20 +0700

    membuat variable untuk mengingat nilai masukkan

---
+++ app/src/main/java/com/example/praktikumkeempat/TeksEdit.kt
@@ -0,0 +1,19 @@
+package com.example.praktikumkeempat
+
+import android.R
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun FormDataDiri(modifier: Modifier
+){
+    //Variabel - variabel untuk mengingat nilai masukan dari keyboard
+    var textName by remember { mutableStateOf("") }
+    var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
+}
