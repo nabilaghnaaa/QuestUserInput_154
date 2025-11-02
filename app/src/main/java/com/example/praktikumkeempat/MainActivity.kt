5699869e14535f979f317c85f01dc12f6443bfa4
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-11-02 17:39:45 +0700

    menggubah main activity

--- app/src/main/java/com/example/praktikumkeempat/MainActivity.kt
+++ app/src/main/java/com/example/praktikumkeempat/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PraktikumKeempatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormPendaftaran(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
