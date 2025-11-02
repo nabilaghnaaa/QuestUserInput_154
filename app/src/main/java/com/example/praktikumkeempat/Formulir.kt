2ba8077607f9c41f8f3cd878f72080c1cb159ff9
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-11-02 17:31:42 +0700

    menambahkan variable dasar

--- app/src/main/java/com/example/praktikumkeempat/Formulir.kt
+++ app/src/main/java/com/example/praktikumkeempat/Formulir.kt
@@ -1,7 +1,16 @@
 package com.example.praktikumkeempat
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 
 @Composable
 fun FormPendaftaran(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
 }
